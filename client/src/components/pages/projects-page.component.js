79a9aeaee3a735091ba02cde21ec209a427ef417
dongsinhho <sinhvua@gmail.com>
2024-06-28 01:19:07 +0700

    feat(fe): update overview

---
+++ client/src/components/pages/projects-page.component.js
@@ -0,0 +1,25 @@
+import './projects-page.component.css'
+import projects_data from '../../mock-data/projects-data'
+
+const Projects = () =>{
+    return (
+        <div className='container'>
+            <div className='main-content'>
+                <div className='page-title'>
+                    <h1>Projects</h1>
+                </div>
+                <div className="project-list">
+                    {projects_data.map((project, index) => (
+                        <div className="project-item" key={index}>
+                            <h3>{project.title}</h3>
+                            <p>{project.description}</p>
+                            <a href={project.link} target="_blank" rel="noopener noreferrer">View Project</a>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects
