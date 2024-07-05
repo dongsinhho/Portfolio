b50bcf02f5e238671b271270a189eca52f43b630
dongsinhho <sinhvua@gmail.com>
2024-07-05 14:17:54 +0700

    feat(fe): divide components

---
+++ client/src/components/molecules/card.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Tag from '../atoms/tag'
+import LinkRef from '../atoms/link-ref'
+import './card.css'
+
+const Card = props => {
+  const {blog} = props
+  return (
+    <div className='card'>
+      <div className='card-content'>
+        <div>
+          <div className='card-time'>
+            <p>{blog.post_date.toLocaleString()}</p>
+            <p>{blog.time_to_read / 60} min read</p>
+          </div>
+          <p className='card-title'>{blog.title}</p>
+          <div className='card-tags'>
+            {blog.tags.map((tag, index) => (
+              <Tag name={tag.name} link={tag.link} referrer key={index}></Tag>
+            ))}
+          </div>
+          <p className='card-description'>{blog.description}</p>
+        </div>
+        <LinkRef link={`blog/${blog.id}`} text="read more →" referrer/>
+      </div>
+    </div>
+  )
+}
+Card.propTypes = {
+  blog: PropTypes.object.isRequired
+}
+
+export default Card
---
+++ client/src/components/organisms/card.section.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './card.section.css'
+import Card from '../molecules/card'
+
+const CardSection = props => {
+    const { blogData } = props
+    return (
+        blogData == null ?
+            <p>No data available</p>
+            :
+            <div className='card-section'>
+                {blogData.slice(0,3).map((blog, index) => (
+                    <Card blog={blog} key={index}/>
+                ))}
+            </div>
+    )
+}
+
+CardSection.propTypes = {
+    blogData: PropTypes.array
+}
+
+export default CardSection
