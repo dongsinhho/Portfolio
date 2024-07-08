1558952edec49c9a504b6fe8411b66b73c6efbff
dongsinhho <sinhvua@gmail.com>
2024-07-09 00:13:59 +0700

    feat(fe):scroll to top, go back  and adjust font more

---
+++ client/src/components/molecules/go-back.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import './go-back.css'
+
+const GoBack = () => {
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate(-1)
+    };
+
+    return (
+        <button onClick={handleGoBack} className='go-back'>
+            <svg xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"
+                ></path>
+            </svg>
+            <span>Go back</span>
+        </button>
+    )
+}
+
+
+export default GoBack
---
+++ client/src/components/molecules/scroll-top.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './scroll-top.css'
+
+const ScrollTop = () => {
+    const handleScrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+    return (
+        <button onClick={handleScrollToTop} className='scroll-top'>
+            <svg xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"
+                ></path>
+            </svg>
+            <span>Back to Top</span>
+        </button>
+    )
+}
+
+export default ScrollTop
