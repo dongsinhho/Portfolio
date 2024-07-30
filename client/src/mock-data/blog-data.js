1515402d7e723b4b59d3dfbfb5a7f2a7af3aecc8
dongsinhho <sinhvua@gmail.com>
2024-07-31 00:38:44 +0700

    feat(fe): add blog

--- client/src/mock-data/blog-data.js
+++ client/src/mock-data/blog-data.js
@@ -35,7 +35,7 @@ const blogs_data = [
             }
         ],
         time_to_read: 300,
-        content: "<p>Hi guys!</p><p>I intended to have my own blog for a long time but kept postponing it for thousand reasons, like lazy, being busy, and side projects. Since I was in school as a child, it's hard to me to take a seat and write down something but somebody once said: \"The journey of a thousand miles begins with one step\" and now I'm starting my journey to record the knowledge I've gathered.</p><p>&nbsp;</p><p>This is to inform myself and anyone who is interested that the upcoming articles will be about computers, programming, networking and <span class=\"Y2IQFc\" lang=\"en\">things that interest me</span>. I'll be sharing knowledge from the projects I've worked on at the companies, as well as personal projects that apply to real life and solve real-life problems.</p><p>&nbsp;</p><p>I hope these articles will not only serve as a valuable resource for others but also as a way for me to continuously learn and stay updated in this fast-evolving field. By documenting and sharing this journey, I aim to create a community of like-minded individuals who are passionate about technology and innovation.</p><p>&nbsp;</p><p>I consider all this knowledge and these experiences to be the luggage I'll carry with me for the next steps!!!</p>"
+        content: `<p>As a first freelance project, I think this is the most suitable topic for my first technical blog.<br>The reason I got this project was through a good-natured colleague of mine. The requirement is to create a windows application which can crawl all video of specific user without watermark. The topic sounds as easy as pie, but when I started doing it, I encountered many difficulties.</p><p>&nbsp;</p><h3>Research</h3><p>First, before I sign off on this, I took about 2 days to research the feasibility of the project. By research on the internet, I found some Tiktok API providers, here is my result:</p><p>Tiktok API for Developer</p><p>This page request developer sign up to get api key and access their data. After sign up and send request, took about 2 days to Tiktok reply. Unfortunately, Tiktok rejected</p><p>Third party API</p><p>Searching more online, I came across sites and have several PoC with Snaptik, Tikwm, Open.tiktokapis.com … , I found that with Open.TiktokApis.com provides api to download tiktok videos (no watermark) with quick response, unlimited access, however videos can only be downloaded based on the video ID.</p><p>So the next problem will be to have a list of user's video info.</p><p>&nbsp;</p><h3>Crawl Tiktok Data and Create WPF app</h3><p>&nbsp;</p><p>With dev tool on MicrosoftEgde we can easy to observe all request/response traffic in website. So I access tiktok.com to get all web data, realize that browser make request to tiktok.com/api/item_list/?… to get list user video with cursor.</p><p>However, Tiktok protects their API by signing URLs, prevent user scrape data, so we need to by pass this.</p><p>Take a litle bit of time, I found a repository of Carcabot which help me by pass this protection by simulate a device to sign on that URLs by using NodeJS with module “playwright-chromium” and then we can get list user video by the legal way.</p><p>&nbsp;</p><p>In parallel with researching ways to scrape tiktok data, I have also started building an application for windows that can scratch tiktok videos and manage videos on the user side.</p><p>About technologies, I choose C# WPF, because it is still a good choice for windows desktop apps for now, we can interfere deeply with the Windows system as well as work well with multithreading. On the other hand, I have experience working with a WPF project at my current and have some knowledge about it.</p><p>&nbsp;</p><p>Still MVVM architecture classical which helps developers create more structured, manageable, and scalable applications.</p><p>I divided structure into separate folder around a main screen, which handle most of the application's tasks. I need to create a really simple app focus on strong and optimal main functions, here is scrape Tiktok videos.</p><p>&nbsp;</p><p>After initializing the project skeleton with some basic functions, the main flow of operations is as follows:</p><p>&nbsp;</p><p>As you can see in the diagram above, we also need user UUID but easily, just only send GET request to tiktok.com/@username,&nbsp;<br>process the returned data with a few steps and we will have the user's information. Furthermore, getting list user video need to run via intermediate device emulation system.</p><p>On AWS, run an Linux EC2 VM and setup an API so windows app can send request and get list user video from that, something like this:</p><p>&nbsp;</p><p>Note: Playwright-chromium run on Linux need to be installed support package.</p><p>&nbsp;</p><p>Now everything quite good. I continue to complete the application by adding storage feature, manage user video and downloading multiple videos in parallel are indispensable. Complete UI with progress bar, sort, filter video, download selective video.</p><p>&nbsp;</p><p>Boom, everything is done, the final step is to package and transfer the project to customers to try and receive feedback.</p><p>In the packaging step, I use Wix toolset - a tool for packaging and creating MSI files, ease of use (some complex custom actions are not :&gt; )</p><p>&nbsp;</p><p>And that's what I had to research and do in my first freelance project. Although the application has many shortcomings and bugs, it is still a small achievement that I took the time to implement.</p><p>Thank you for reading !!!</p><p>&nbsp;</p><p>&nbsp;</p>`
     },
     {
         id: 2,
@@ -57,63 +57,6 @@ const blogs_data = [
             }
         ],
         time_to_read: 300,
-        content: ` <h2>Introduction</h2>
-        <p>In this blog post, I will share the process of building a TikTok crawler application using C#. This application allows you to scrape TikTok videos and user data for analysis and other purposes.</p>
-
-        <h2>Prerequisites</h2>
-        <p>Before we start, ensure you have the following tools and libraries installed:</p>
-        <ul>
-            <li>Visual Studio</li>
-            <li>.NET Framework</li>
-            <li>HtmlAgilityPack</li>
-            <li>Newtonsoft.Json</li>
-        </ul>
-
-        <h2>Step 1: Setting Up the Project</h2>
-        <p>First, create a new C# project in Visual Studio. Choose a Console Application for simplicity. Name your project "TikTokCrawler".</p>
-
-        <h2>Step 2: Installing Required Libraries</h2>
-        <p>To install HtmlAgilityPack and Newtonsoft.Json, use the NuGet Package Manager. Open the Package Manager Console and run the following commands:</p>
-        <pre>
-            Install-Package HtmlAgilityPack
-            Install-Package Newtonsoft.Json
-        </pre>
-
-        <h2>Step 3: Writing the Crawler Logic</h2>
-        <p>Next, write the logic to fetch and parse TikTok data. Here is a simple example to get you started:</p>
-        <pre>
-            using System;
-            using System.Net.Http;
-            using HtmlAgilityPack;
-            using Newtonsoft.Json.Linq;
-
-            namespace TikTokCrawler
-            {
-                class Program
-                {
-                    static async System.Threading.Tasks.Task Main(string[] args)
-                    {
-                        string url = "https://www.tiktok.com/@someusername";
-                        HttpClient client = new HttpClient();
-                        var response = await client.GetStringAsync(url);
-
-                        HtmlDocument doc = new HtmlDocument();
-                        doc.LoadHtml(response);
-
-                        var scripts = doc.DocumentNode.SelectNodes("//script[@type='application/json']");
-                        foreach (var script in scripts)
-                        {
-                            var json = script.InnerText;
-                            var data = JObject.Parse(json);
-                            Console.WriteLine(data);
-                        }
-                    }
-                }
-            }
-        </pre>
-
-        <h2>Conclusion</h2>
-        <p>With the steps outlined above, you can build a simple TikTok crawler application using C#. This basic example demonstrates fetching and parsing data, but you can extend it to collect specific information and handle more complex scenarios.</p>`
     },
     {
         id: 3,
