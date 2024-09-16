44d0c08b5155ad395b1207c31fecad4baa5ad661
dongsinhho <sinhvua@gmail.com>
2024-09-16 23:43:11 +0700

    feat(Integrate) update

--- client/src/components/pages/admin-page.component.jsx
+++ client/src/components/pages/admin-page.component.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import blogs_data from '../../mock-data/blog-data'
 import { useNavigate } from 'react-router-dom'
 import LinkRef from '../atoms/link-ref';
 import '../../styles/pages-style/admin-page.component.css'
+import useAxios from '../../hooks/useAxios';
+import { HandleLogout } from '../../api/UserApi';
+import { DataContext } from '../../context/DataProvider';
 
 const Admin = () => {
-
+    const axios = useAxios();
     const navigate = useNavigate();
+    const dataContext = useContext(DataContext)
+    const [, setAccessToken] = dataContext.token
 
     const handleAddNewBlog = () => {
         navigate('/write')
+    }
+
+    const handleLogout = () => {
+        HandleLogout(axios)
+        localStorage.removeItem('accessToken')
+        axios.deleteRefreshTokenFromCookie()        
+        setAccessToken('')
     }
 
     return (
@@ -17,7 +29,10 @@ const Admin = () => {
             <div className='page-title'>
                 <h1>Blog manager</h1>
             </div>
-            <button className='add-blog' onClick={handleAddNewBlog}> ADD NEW BLOG</button>
+            <div className='list-button'>
+                <button className='admin-button' onClick={handleAddNewBlog}>New blog</button>
+                <button className='admin-button' onClick={handleLogout}>Log out</button>
+            </div>
             <div className="admin-blog-list">
                 {blogs_data.map((blog, index) => (
                     <div className="admin-blog-item" key={index}>
